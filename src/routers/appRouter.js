cb3000ef61b40330d0cd181d0f3fb1bb26840992
dscrane <daegancrane@gmail.com>
2020-08-01 07:22:37 -0400

    got the landing page redirection when authenticated set up and working

--- src/routers/appRouter.js
+++ src/routers/appRouter.js
@@ -4,11 +4,11 @@ const router = new express.Router();
 
 router.get('/', (req, res) => {
   console.log('\x1b[31m Auth Token \x1b[0m', req.cookies.userAuth);
-  // if (!req.cookies.userAuth) {
-  res.render('home', { buttons: ['Register', 'Log In'] });
-  // }
+  if (req.cookies.userAuth === undefined) {
+    return res.render('home', { buttons: ['Register', 'Log In'] });
+  }
 
-  // res.redirect('/users/profile');
+  res.redirect('/users/profile');
 });
 
 module.exports = router;
